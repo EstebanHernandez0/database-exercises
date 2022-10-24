fbbac96ed92578ee63cca1277b1bb8f9cdef1e12
EstebanHernandez0 <EstebanHernandez2024@gmail.com>
2022-10-24 13:59:40 -0500

    Added tables exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,17 @@
+SHOW tables;
+SELECT NULL FROM employees;
+
+/*Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
+	Tables employees and salaries have numeric columns*/
+	
+/*Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
+	Tables titles, employees, and departments have strings*/
+
+/*Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
+	Tables dept_emp, dept_managers, employees, salaries, and titles have date type columns*/
+	
+/*What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
+	They all have a primary key*/
+	
+/*Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
+	SHOW CREATE TABLE dept_manager;*/

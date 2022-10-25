f90a91ebd85897d451151af135d8c7b28af822ae
EstebanHernandez0 <EstebanHernandez2024@gmail.com>
2022-10-25 10:28:59 -0500

    My SQL select exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,15 +1,15 @@
 /*Explore the structure of the albums table.
 
 a. How many rows are in the albums table?
-	There are 6 rows
+	SELECT count(*) from albums;
 
 b. How many unique artist names are in the albums table?
 	There are 22 unique artist names
- SELECT DISTINCT artist
+ SELECT COUNT(DISTINCT artist)
  FROM albums;
 
 c. What is the primary key for the albums table?
-	
+	DESCRIBE album
 
 d. What is the oldest release date for any album in the albums table? What is the most recent release date?
 	The oldest realse date is 1967, and the most recent release date is 2011
@@ -17,7 +17,6 @@ d. What is the oldest release date for any album in the albums table? What is th
 SELECT release_date
 FROM albums
 ORDER by release_date;
-
 
 
 Write queries to find the following information:
@@ -44,18 +43,20 @@ WHERE name = 'Nevermind';
 
 d. Which albums were released in the 1990s
 
-SELECT name
+SELECT name, release_date
 FROM albums
 WHERE release_date >= 1990 and release_date < 2000;
 
 
 e. Which albums had less than 20 million certified sales
 
-SELECT sales
+SELECT name, sales
 FROM albums
 WHERE sales < 20;
 
 f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
-	Because SQL is specific when doing a query
+	SELECT genre 
+	 FROM albums 
+	 WHERE genre = "Rock";
 
 */

3609cc0a03f459b7d27cee745a1fed4a37eb8bf8
Esteban Hernandez <116030924+EstebanHernandez0@users.noreply.github.com>
2022-10-25 14:11:20 -0500

    Update Where.exercises.sql

--- Where.exercises.sql
+++ Where.exercises.sql
@@ -1,25 +1,31 @@
 
--- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. Enter a comment with the number of records returned.   709 records found
+-- 2. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. Enter a comment with the number of records returned.   
+--709 records found
 
 SELECT * FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
--- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. Enter a comment with the number of records returned. Does it match number of rows from Q2?  709 records found
+-- 3. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. Enter a comment with the number of records returned. Does it match number of rows from Q2?  
+--709 records found
 
 SELECT * FROM employees
 WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
--- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. Enter a comment with the number of records returned.  619 records found
+-- 4. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. Enter a comment with the number of records returned.  
+--441 records found
 
 SELECT * FROM employees
-WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' AND gender = 'F';
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+  AND gender = 'F';
 
--- Find all current or previous employees whose last name starts with 'E'. Enter a comment with the number of employees whose last name starts with E.    7330 records found
+-- 5. Find all current or previous employees whose last name starts with 'E'. Enter a comment with the number of employees whose last name starts with E.    
+--7330 records found
 
 SELECT * FROM employees
 WHERE last_name LIKE 'E%';
 
--- Find all current or previous employees whose last name starts or ends with 'E'. Enter a comment with the number of employees whose last name starts or ends with E. How many employees have a last name that ends with E, but does not start with E? 30723 employees last name start or end with "Ee"
+-- 6. Find all current or previous employees whose last name starts or ends with 'E'. Enter a comment with the number of employees whose last name starts or ends with E. How many employees have a last name that ends with E, but does not start with E? 
+--30723 employees last name start or end with "Ee"
 
 SELECT * FROM employees
 WHERE last_name LIKE 'E%' OR last_name LIKE'%e';
@@ -27,36 +33,36 @@ WHERE last_name LIKE 'E%' OR last_name LIKE'%e';
 SELECT * FROM employees
 WHERE last_name LIKE'%e';
 
--- Find all current or previous employees employees whose last name starts and ends with 'E'. Enter a comment with the number of employees whose last name starts and ends with E. How many employees' last names end with E, regardless of whether they start with E? 899 records found
+-- 7. Find all current or previous employees employees whose last name starts and ends with 'E'. Enter a comment with the number of employees whose last name starts and ends with E. How many employees' last names end with E, regardless of whether they start with E? 899 records found
 
 SELECT * FROM employees
-WHERE last_name LIKE 'E%' AND last_name LIKE'%e';
-
-SELECT * FROM employees
-WHERE last_name LIKE '%e' AND last_name  NOT LIKE'E%';
+WHERE last_name LIKE 'E%e';
 
 
--- Find all current or previous employees hired in the 90s. Enter a comment with the number of employees returned. 135214 RECORDS FOUND
+-- 8. Find all current or previous employees hired in the 90s. Enter a comment with the number of employees returned. 
+--135214 RECORDS FOUND
 
 SELECT * FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'; 
 
--- Find all current or previous employees born on Christmas. Enter a comment with the number of employees returned.
+-- 9. Find all current or previous employees born on Christmas. Enter a comment with the number of employees returned.
+-- 842 employees born on Christmas day
 SELECT * FROM employees
-WHERE hire_date Like '%-12-25';
+WHERE birth_date Like '%-12-25';
 
--- Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with the number of employees returned. 2 records found
-
+-- 10 Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with the number of employees returned. 2 records found
+-- 362 records returned 
 SELECT * FROM employees
-WHERE hire_date Like '%-12-25' AND birth_date LIKE '%-12-25';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+  and birth_date LIKE '%-12-25';
 
--- Find all current or previous employees with a 'q' in their last name. Enter a comment with the number of records returned. 1873 records found
-
+-- 11. Find all current or previous employees with a 'q' in their last name. Enter a comment with the number of records returned. 1873 records found
+-- 1873 records returned 
 SELECT * FROM employees
-WHERE last_name LIKE'%q%';
+WHERE last_name LIKE '%q%';
 
--- Find all current or previous employees with a 'q' in their last name but not 'qu'. How many employees are found? 547 records found
-
+-- 12. Find all current or previous employees with a 'q' in their last name but not 'qu'. How many employees are found? 547 records found
+--547 empolyees
 SELECT * FROM employees
 WHERE last_name LIKE'%q%' AND last_name NOT LIKE '%qu%';
 
